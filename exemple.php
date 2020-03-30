dad92de3d37d49e067181d9510e174d7f53f1c3a
Xorrad <36533226+Xorrad@users.noreply.github.com>
2020-03-30 21:11:25 +0200

    Update exemple.php

--- exemple.php
+++ exemple.php
@@ -4,11 +4,11 @@ include('reqx.php');
 
 $bdd = new PDO("mysql:host=yourhost.com;dbname=yourdatabasename", 'yourusername', 'yourpassword');
 
-$req = req_comp("SELECT * FROM users WHERE Id = ? AND Username = ?", array(1, "Xorrad"));
+$req = reqx("SELECT * FROM users WHERE Id = ? AND Username = ?", array(1, "Xorrad"));
 echo 'Row Count: '.$req[1].'<br>';
 echo 'Is Premium: '.$req[0]['Premium'].'<br>';
 
 //ENABLE DEBUG MOD
 //an error message will appear because the "userse" table does not exist.
-$req = req_comp("SELECT * FROM userse WHERE Id = ?", array(1), true);
+$req = reqx("SELECT * FROM userse WHERE Id = ?", array(1), true);
 ?>
